17fe2a083934e5805a888bd6c76f46fddea8d716
LorcanaChecker <balsky@gmail.com>
2026-06-26 14:40:01 +0200

    Update check.py

--- check.py
+++ check.py
@@ -2,12 +2,17 @@ import os
 import requests
 
 BOT_TOKEN = os.environ["BOT_TOKEN"].strip()
+CHAT_ID = os.environ["CHAT_ID"].strip()
 
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/getMe"
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-print("URL:", url[:45] + "...")
-
-response = requests.get(url)
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "🎉 Testbericht vanaf GitHub!"
+    }
+)
 
 print("Status:", response.status_code)
 print("Response:", response.text)
