5cb26d005a7aa95c723c46687836cc26ace74529
LorcanaChecker <balsky@gmail.com>
2026-06-26 14:16:26 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,1 +1,16 @@
-print("Hallo! GitHub Actions werkt.")
+import os
+import requests
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+message = "✅ Testbericht vanaf GitHub Actions! Alles werkt."
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Bericht verzonden!")
